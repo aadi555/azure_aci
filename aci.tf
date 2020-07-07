be4ba8b46b4305dc90d9824e102eb0082223ddcf
aadi555 <38933135+aadi555@users.noreply.github.com>
2020-07-07 23:12:58 +0530

    Add files via upload

---
+++ aci.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "aci" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
+
+resource "azurerm_container_group" "aci_cg" {
+  name                  = "${var.container_group_name}"
+  resource_group_name   = "${azurerm_resource_group.aci_rg.name}"
+  location              = "${azurerm_resource_group.aci_rg.location}"
+  ip_address_type       = "public"
+  dns_name_label        = "${var.dns_name_label}"
+  os_type               = "${var.os_type}"
+
+  container {
+      name      = "${var.container_name}"
+      image     = "${var.image_name}"
+      cpu       = "${var.cpu_core_number}"
+      memory    = "${var.memory_size}"
+      port      = "${var.port_number}"
+  }
+  tags = {
+    app = var.app_name
+    env = var.env_name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "container_group_name" { }
+
+variable "resource_group_name" { }
+
+variable "location" { }
+
+variable "dns_name_label" { }
+
+variable "os_type" { }
+
+variable "container_name" { }
+
+variable "image_name" { }
+
+variable "cpu_core_number" { }
+
+variable "memory_size" { }
+
+variable "port_number" { }
+
+variable "app_name" { }
+
+variable "env_name" { }
