5a383f3b51198df4a23037e52ab997ca5626da32
aadi555 <38933135+aadi555@users.noreply.github.com>
2020-07-07 23:35:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,18 +6,32 @@ variable "location" { }
 
 variable "dns_name_label" { }
 
-variable "os_type" { }
+variable "os_type" {
+  default   = "linux"
+  description   = "The type of container os"}
 
 variable "container_name" { }
 
 variable "image_name" { }
 
-variable "cpu_core_number" { }
+variable "cpu_core_number" {
+  default       = "1"  
+  description   = "The required number of CPU cores of the containers"
+}
 
-variable "memory_size" { }
+variable "memory_size" {
+  default       = "2"  
+  description   = "The required memory of the containers in GB"
+}
 
-variable "port_number" { }
-
+variable "port_number" {
+  default       = "22"  
+  description   = "A public port for the container"
+}
+variable "restart_policy" { 
+  default  = "onFailure"
+  description = "Container to restart the based the specified defaults"
+}
 variable "app_name" { }
 
 variable "env_name" { }
